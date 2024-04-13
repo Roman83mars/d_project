cc4b7cb27d208ca7854016b4c90a6a1eafa227ce
Roman Amb <roman83mars@gmail.com>
2024-04-14 01:15:31 +0300

    slider complete

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './src/css/style.css'
 
 import { modal } from './src/modules/modal.js';
 import { smoothScroll } from './src/modules/smoothScroll.js';
+import { slider } from './src/modules/slider.js';
 
 modal()
 smoothScroll()
+slider()
---
+++ src/modules/slider.js
@@ -0,0 +1,91 @@
+export const slider = () => {
+    const sliderBlock = document.querySelector('.top-slider')
+    const slides = document.querySelectorAll('.item')
+
+    const timeInterval = 3000
+
+    let currentSlide = 0
+    let interval
+
+    const addDots = () => {
+        sliderBlock.insertAdjacentHTML('beforeend', '<ul class="dots"></ul>')
+        const dots = document.querySelector('.dots')
+        slides.forEach((dot, index) => {
+            dot = document.createElement('li')
+            dot.classList.add('dot')
+
+            if (index == 0) {
+                dot.classList.add('dot-active')
+            }
+            dots.append(dot)
+        })
+    }
+    addDots()
+
+    const dots = document.querySelectorAll('.dot')
+
+    const prevSlide = (elems, index, strClass) => {
+        elems[index].classList.remove(strClass)
+    }
+    const nextSlide = (elems, index, strClass) => {
+        elems[index].classList.add(strClass)
+    }
+    const autoSlide = () => {
+        prevSlide(slides, currentSlide, 'item-active')
+        prevSlide(dots, currentSlide, 'dot-active')
+        currentSlide++
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0
+        }
+        nextSlide(slides, currentSlide, 'item-active')
+        nextSlide(dots, currentSlide, 'dot-active')
+    }
+    const startSlide = (timer) => {
+        interval = setInterval(autoSlide, timer)
+    }
+    const stopSlide = () => {
+        clearInterval(interval)
+    }
+
+    sliderBlock.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        if (!e.target.matches('.dot')) {
+            return
+        }
+
+        prevSlide(slides, currentSlide, 'item-active')
+        prevSlide(dots, currentSlide, 'dot-active')
+
+        if (e.target.classList.contains('dot')) {
+            dots.forEach((dot, index) => {
+                if (e.target === dot) {
+                    currentSlide = index
+                }
+            })
+        }
+        if (currentSlide >= slides.length) {
+            currentSlide = 0
+        }
+        if (currentSlide < 0) {
+            currentSlide = slides.length - 1
+        }
+
+        nextSlide(slides, currentSlide, 'item-active')
+        nextSlide(dots, currentSlide, 'dot-active')
+    })
+
+    sliderBlock.addEventListener('mouseenter', (e) => {
+        if (e.target.matches('.dot')) {
+            stopSlide()
+        }
+    }, true)
+    sliderBlock.addEventListener('mouseleave', (e) => {
+        if (e.target.matches('.dot')) {
+            startSlide(timeInterval)
+        }
+    }, true)
+
+    startSlide(timeInterval)
+}
